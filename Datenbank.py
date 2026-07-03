3d8b9693c35a4b74350549253a90418a49e3b665
SERW <bjoern.lehmann@iu-study.org>
2026-07-03 20:10:21 +0200

    Code cleanup

--- Datenbank.py
+++ Datenbank.py
@@ -20,14 +20,10 @@ class Datenbank:
     def _tabellen_erstellen(self) -> None:
         """Erstellt Tabellen in Datenbank, falls diese noch nicht existieren"""
         with self._connection:
-            self._connection.executescript(
-                'CREATE TABLE IF NOT EXISTS studium(id INTEGER PRIMARY KEY AUTOINCREMENT, studiengang TEXT NOT NULL, hochschule TEXT NOT NULL, start_datum date NOT NULL, geplantes_end_datum date NOT NULL);')
-            self._connection.executescript(
-                'CREATE TABLE IF NOT EXISTS semester (id INTEGER PRIMARY KEY AUTOINCREMENT, studium_id INTEGER NOT NULL, nummer INTEGER NOT NULL, FOREIGN KEY (studium_id) REFERENCES studium(id) ON DELETE CASCADE);')
-            self._connection.executescript(
-                'CREATE TABLE IF NOT EXISTS modul (id INTEGER PRIMARY KEY AUTOINCREMENT, semester_id INTEGER NOT NULL, kuerzel TEXT NOT NULL, name TEXT NOT NULL, etcs INTEGER NOT NULL, FOREIGN KEY (semester_id) REFERENCES semester(id) ON DELETE CASCADE);')
-            self._connection.executescript(
-                'CREATE TABLE IF NOT EXISTS pruefung (id INTEGER PRIMARY KEY AUTOINCREMENT, modul_id INTEGER NOT NULL UNIQUE, note FLOAT(1,2) NOT NULL, FOREIGN KEY (modul_id) REFERENCES modul(id) ON DELETE CASCADE);')
+            self._connection.executescript('CREATE TABLE IF NOT EXISTS studium(id INTEGER PRIMARY KEY AUTOINCREMENT, studiengang TEXT NOT NULL, hochschule TEXT NOT NULL, start_datum date NOT NULL, geplantes_end_datum date NOT NULL);')
+            self._connection.executescript('CREATE TABLE IF NOT EXISTS semester (id INTEGER PRIMARY KEY AUTOINCREMENT, studium_id INTEGER NOT NULL, nummer INTEGER NOT NULL, FOREIGN KEY (studium_id) REFERENCES studium(id) ON DELETE CASCADE);')
+            self._connection.executescript('CREATE TABLE IF NOT EXISTS modul (id INTEGER PRIMARY KEY AUTOINCREMENT, semester_id INTEGER NOT NULL, kuerzel TEXT NOT NULL, name TEXT NOT NULL, etcs INTEGER NOT NULL, FOREIGN KEY (semester_id) REFERENCES semester(id) ON DELETE CASCADE);')
+            self._connection.executescript('CREATE TABLE IF NOT EXISTS pruefung (id INTEGER PRIMARY KEY AUTOINCREMENT, modul_id INTEGER NOT NULL UNIQUE, note FLOAT(1,2) NOT NULL, FOREIGN KEY (modul_id) REFERENCES modul(id) ON DELETE CASCADE);')
 
     def read_one(self, query: str) -> sqlite3.Row | None:
         """Erhält eine SQL-Query als string und gibt ein oder kein Ergebnis zurück"""
@@ -50,5 +46,4 @@ class Datenbank:
         Speichert Studium-Objekt in der Datenbank.
         Extra-Methode, weil es mehrere Aufrufe gibt.
         """
-        self.write(
-            f'UPDATE studium SET studiengang="{studium.studiengang}", hochschule="{studium.hochschule}", start_datum="{studium.start_datum}", geplantes_end_datum="{studium.geplantes_end_datum}";')
+        self.write(f'UPDATE studium SET studiengang="{studium.studiengang}", hochschule="{studium.hochschule}", start_datum="{studium.start_datum}", geplantes_end_datum="{studium.geplantes_end_datum}";')
